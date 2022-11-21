d44621823419effd8e966f7d87c82a8c693cf5e2
Nicolas <mijaelquil2001@gmail.com>
2022-11-21 10:59:31 +0200

    ok

--- src/components/cars/cars.jsx
+++ src/components/cars/cars.jsx
@@ -1,23 +1,15 @@
-import axios from 'axios'
 import React from 'react'
-import { useState } from 'react'
 import { useEffect } from 'react'
 
 const Cars = () => {
-    const [cars, setcars] = useState([])
+
     const doApi = async () => {
-        let url = "https://project-yarin.herokuapp.com/cars?perPage=99"
-        const resp = await axios.get(url)
-        const data = await resp.data
-        console.log(data)
-        setcars(data)
+        let url = `https://project-yarin.herokuapp.com/cars?perPage=99`
+
+
     }
-    useEffect(() => {
-        doApi();
-    }, [])
     return (
         <div>
-            {cars[0]?.company}
 
         </div>
     )
