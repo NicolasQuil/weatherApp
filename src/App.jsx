ccef13ccda27026001a3307fe9a527641e4e9d3d
Nicolas <mijaelquil2001@gmail.com>
2022-11-28 13:12:07 +0200

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
 import './App.css';
 import Cars from './components/cars/cars';
-import Crypto from './components/crypto/crypto';
-
-
 
 function App() {
   return (
     <div >
-      <Crypto/>
-      {/* <Cars/> */}
+     <Cars/>
     </div>
   );
 }
--- src/components/cars/cars.jsx
+++ src/components/cars/cars.jsx
@@ -1,18 +1,45 @@
 import axios from 'axios'
 import React, { useState } from 'react'
+import { useRef } from 'react';
 import { useEffect } from 'react'
 import Car from './car';
 
 const Cars = () => {
     const [cars, setCars] = useState([]);
     // const [data,setData] = useState();
+    const [categories, setCategories] = useState([]);
+    const [category, setCategory] = useState("All")
+    const selectRef = useRef()
+
+    const getCategories = (_data) => {
+        const cat = []
+        cat.push("ALL")
+
+        _data.forEach(item => {
+            if (!cat.includes(item.category)) {
+                cat.push(item.category);
+            }
+        });
+        setCategories(cat)
+        console.log(cat)
+    }
 
     const doApi = async () => {
         try {
             let url = `https://project-yarin.herokuapp.com/cars?perPage=99`
             const { data } = await axios(url)
             console.log(data)
-            setCars(data);
+            getCategories(data);
+            let carsFiltered = [...data]
+            if (category != "ALL" && selectRef.current.value) {
+                carsFiltered = data.filter(item => item.category === category)
+            }
+            else {
+
+                carsFiltered = [...data]
+            }
+
+            setCars(carsFiltered);
 
             // const resp = await axios(url)
             // setCars(resp.data)
@@ -21,24 +48,38 @@ const Cars = () => {
             console.log(err.response)
         }
     }
+    console.log(category)
 
     //בשביל השאפליקציה תאתחל את עצמה ביחד עם הקשת האייפיאי נשתמש בהוק זה:
     useEffect(() => {
         doApi()
-    }, [])
+
+    }, [category])
 
     return (
         <div>
 
-            <h1 className='display-3 text-center'>Cars:</h1>
+            <h1 className='display-3 text-center'>Cars</h1>
 
             <div className="container">
+
+                <div className='col-lg-3 col-10 mx-auto'>
+
+                    <select ref={selectRef} onChange={() => {
+                        setCategory(selectRef.current.value)
+                    }} className='form-select'>
+                        {categories?.map((item, i) => {
+                            return (
+                                <option key={i} value={item}>{item.toUpperCase()}</option>
+                            )
+                        })}
+                    </select>
+                </div>
                 <div className="row m-0 p-0">
                     {/* Render All Cars */}
                     {cars?.map((item, i) => {
                         return (
-                            <Car key={i} car={item} />
-
+                            <Car key={i} number={i} car={item} />
                         )
                     })}
                     {/* Render All Cars */}
@@ -46,13 +87,12 @@ const Cars = () => {
 
             </div>
 
+
             {/* {cars.map((item,i)=>
                 (
                     <p>{item.model}</p>
                 )
             )} */}
-
-
 
             {/* {cars[0]?.model ?
                 <div>
